18dfa240e3c3db3b05618614d481d0e76b1c2142
Yu-Sung Chang <yusung@yusungchang.com>
2026-02-17 22:43:41 -0800

    Fetch tags in install.sh to ensure correct versioning

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Install script for snd - stamps version from Git and copies to ~/.local/bin
 
+git fetch --tags  # Ensure tags are up to date
 VERSION=$(git describe --tags --always 2>/dev/null || echo "dev")
 cp snd ~/.local/bin/snd
 sed -i.bak "s/@@VERSION@@/$VERSION/" ~/.local/bin/snd
