a54ab1fce567e533e9344401c167340748311812
Yu-Sung Chang <yusung@yusungchang.com>
2026-02-17 22:40:40 -0800

    Add version stamping with Git tags and install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install script for snd - stamps version from Git and copies to ~/.local/bin
+
+VERSION=$(git describe --tags --always 2>/dev/null || echo "dev")
+cp snd ~/.local/bin/snd
+sed -i.bak "s/@@VERSION@@/$VERSION/" ~/.local/bin/snd
+echo "Installed snd $VERSION to ~/.local/bin/snd"
